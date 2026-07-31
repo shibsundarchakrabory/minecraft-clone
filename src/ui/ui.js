3c7a55d9723bb9ddaa300f57fbd9138dc07419fc
SHIBSUNDAR CHAKRABORTY <shibsundarchakra9@gmail.com>
2026-07-31 09:11:04 +0530

    teraign  controls added

--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,20 +1,19 @@
-import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
-
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min.js";
 
 export default function createUI(world) {
     const gui = new GUI();
 
-    gui.add(world.size, 'width', 1, 64, 1).name('Width')
+    gui.add(world.size, "width", 1, 64, 1).name("Width");
 
-    gui.add(world.size, 'height', 1, 32, 1).name('Height')
+    gui.add(world.size, "height", 1, 32, 1).name("Height");
 
-    
+    const terrainFolder = gui.addFolder("Terrain");
+
+    terrainFolder.add(world.parameters.terrain, "scale", 10, 100).name("scale");
+    terrainFolder.add(world.parameters.terrain, "magnitude", 0, 1).name("magnitude");
+    terrainFolder.add(world.parameters.terrain, "offset", 0, 1).name("offset");
 
     gui.onChange(() => {
         world.generate();
-    })
-
-
-
-
+    });
 }
